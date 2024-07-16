6021c0c3a657f27ebd1319f4b8480ef04cd3a4d6
Eol <eol@eolstudy.com>
2024-07-16 16:29:03 +0900

    M Screen1: add webcam streaming

--- BNF/src/routes/Screen1.svelte
+++ BNF/src/routes/Screen1.svelte
@@ -1,10 +1,9 @@
 <script>
     import { onMount } from 'svelte';
-  
+
     // 表示データ読み込み
-    let videoUrls = ["stream1.m3u8", "stream2.m3u8", "stream3.m3u8"];
     let jsonData = {};
-    let videoElement;
+    let videoWin, videoDesk;
 
     // JSONデータの例
     onMount(async () => {
@@ -15,12 +14,19 @@
 
      /******************** Streaming  *******************************/
      function startStream() {
-        const img = document.createElement('img');
-        img.src = "http://localhost:8000/video_feed";
-        img.alt = "Video Stream";
-        img.style.width = "100%";
-        img.style.height = "auto";
-        videoElement.appendChild(img);
+        const imgWindow = document.createElement('img');
+        imgWindow.src = "http://blue-network.eolstudy.com/cam-window";
+        imgWindow.alt = "Video Stream";
+        imgWindow.style.width = "100%";
+        imgWindow.style.height = "auto";
+        videoWin.appendChild(imgWindow);
+
+        const imgDesk = document.createElement('img');
+        imgDesk.src = "http://blue-network.eolstudy.com/cam-desk";
+        imgDesk.alt = "Video Stream";
+        imgDesk.style.width = "100%";
+        imgDesk.style.height = "auto";
+        videoDesk.appendChild(imgDesk);
     }
 
     /******************** OnMount *******************************/
@@ -29,22 +35,23 @@
       startStream();
     });
 </script>
-  
+
   <div class="grid-container">
     <div class="grid-item">
-        <div id="videoContainer" bind:this={videoElement}></div> <!--とりあえずwebカメラの映像表示-->
-    <!--
-      <video controls autoplay>
-        <source src={videoUrls[0]} type="application/x-mpegURL">
-        Your browser does not support the video tag.
-      </video>
-    -->
+      <div id="video-win" bind:this={videoWin}></div> <!--とりあえずwebカメラの映像表示-->
+        <!-- <video controls autoplay>
+          <source src={videoUrls[0]} type="application/x-mpegURL">
+          Your browser does not support the video tag.
+        </video> -->
+    </div>
+    <div class="grid-item">
+      <div id="video-desk" bind:this={videoDesk}></div>
     </div>
     <div class="grid-item json-display">
       <pre>{JSON.stringify(jsonData, null, 2)}</pre>
     </div>
   </div>
-  
+
   <style>
     .grid-container {
       display: grid;
@@ -53,27 +60,27 @@
       gap: 10px;
       padding: 10px;
     }
-  
+
     .grid-item {
       border: 2px solid #ccc;
       padding: 10px;
     }
-  
+
     video {
       width: 100%;
       height: auto;
     }
-  
+
     .json-display {
       overflow: auto;
       background-color: #f5f5f5;
       padding: 10px;
       border-radius: 5px;
     }
-  
+
     pre {
       margin: 0;
       white-space: pre-wrap;
       word-wrap: break-word;
     }
-  </style>  
+  </style>
