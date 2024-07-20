dc313420aa6c043e7e6ceb80fca9fee8b5f1b5ad
Eol <eol@eolstudy.com>
2024-07-21 04:30:19 +0900

    Backend: alarm module

--- complete/Backend/alarm.py
+++ complete/Backend/alarm.py
@@ -1,2 +1,14 @@
+import socket
+
 def play_alarm(pi_id: int):
-    pass
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    port = 58881 if pi_id == 1 else 58882
+    sock.connect(('localhost', port))
+    try:
+        sock.sendall(b'play alarm now')
+    finally:
+        sock.close()
+
+if __name__ == '__main__':
+    play_alarm(1)
+
