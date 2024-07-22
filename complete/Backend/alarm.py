2f6012b6b4d606e4a19d26aa6f1799e0480c11d1
Eol <eol@eolstudy.com>
2024-07-22 19:41:08 +0900

    Alarm module: change host domain name

--- complete/Backend/alarm.py
+++ complete/Backend/alarm.py
@@ -3,7 +3,7 @@ import socket
 def play_alarm(pi_id: int):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     port = 58881 if pi_id == 1 else 58882
-    sock.connect(('localhost', port))
+    sock.connect(('host.docker.internal', port))
     try:
         sock.sendall(b'play alarm now')
         print(f'[INFO] [ALARM] alarm played for {['pi-window', 'pi-desk'][pi_id - 1]}')
